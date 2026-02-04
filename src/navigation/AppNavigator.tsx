6218a2cafdd1a067ebc5c82fce84b8696c41692c
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-02-04 10:10:00 +0300

    refactor(nav): remove redundant detail screen registration from root navigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -9,7 +9,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SplashScreen } from '../screens/common/SplashScreen';
 import { MainTabNavigator } from './MainTabNavigator';
 import { AuthNavigator } from './AuthNavigator';
-import { AnnouncementDetailScreen } from '../screens/announcements/AnnouncementDetailScreen';
 import { useAuthStore } from '../store/authStore';
 import { RootStackParamList } from '../types/navigation';
 
@@ -29,7 +28,6 @@ export const AppNavigator: React.FC = () => {
                 <Stack.Screen name="Splash" component={SplashScreen} />
                 <Stack.Screen name="Auth" component={AuthNavigator} />
                 <Stack.Screen name="Main" component={MainTabNavigator} />
-                <Stack.Screen name="AnnouncementDetail" component={AnnouncementDetailScreen} />
                 <Stack.Screen name="Profile" component={ProfileScreen} />
             </Stack.Navigator>
         </NavigationContainer>
